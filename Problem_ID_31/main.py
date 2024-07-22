f5520521bca9c9141f9af8af73b04b3c33eb3651
VaclavHa <hanzlvaclav00@gmail.com>
2024-07-22 18:51:27 +0200

    Changes to the main.py adding the functionality

--- Problem_ID_31/main.py
+++ Problem_ID_31/main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+INPUT_DATA = "input.txt"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_and_parse_data(input_data: str):
+    """
+    Parse data to proceed with rotation
+    :param input_data: file with lines of data
+    :return: List of split data to number and string to rotate
+    """
+    parsed_data = list()
+    with open(input_data, mode='r') as file:
+        for line in file.readlines():
+            parsed_line = line.strip().split()
+            parsed_data.append((int(parsed_line[0]), parsed_line[1]))
+    return parsed_data
 
 
-# Press the green button in the gutter to run the script.
+def rotate_string(data: list):
+    """
+    Rotate string
+    :param data: list of parsed data
+    :return: list of rotated strings
+    """
+    rotated_strings = list()
+    for steps, string in data:
+        looped_string = ""
+        if steps > 0:
+            for letter in string[:steps]:
+                looped_string += letter
+            rotated_strings.append(string[steps:] + looped_string)
+        if steps < 0:
+            new_string = string[steps:]
+            rotated_strings.append(new_string + (string[:(len(string) - abs(steps))]))
+    return rotated_strings
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    data_to_proceed = get_and_parse_data(INPUT_DATA)
+    result = rotate_string(data_to_proceed)
+    print(" ".join(result))
